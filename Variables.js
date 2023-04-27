01ffbb80e8d12ca707fdadc35bac0899c045eed5
Nicolas Marquez <nicolas.marquez.casas@gmail.com>
2023-04-27 16:52:08 -0300

    Tipos de datos Java

---
+++ Variables.js
@@ -0,0 +1,27 @@
+// String
+
+var bootcamp = 'Henry';
+
+// Números
+
+var numeroEntero = 17;
+
+var numeroNegativo = -4;
+
+var numeroDecimal = 11.75;
+
+// Booleanos
+
+var estoyEntendiendo = true;
+
+var estoyAburrido = false;
+
+// Undefined
+
+var cajon1;
+
+console.log(cajón1) ;
+
+// Null
+
+var sinNada = null;
